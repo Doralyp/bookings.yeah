a27e086e147db076c1292519fc7d1f3a49e354c0
Doraly Pantaleon <doralypantaleon@gmail.com>
2015-07-17 00:46:12 -0400

    created seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# Creates users
+10.times do
+  User.create!(name: Faker::Name.name)
+end
+
+# Creates hotels
+hotels = []
+10.times do
+  hotels << Hotel.create!(name: Faker::Company.name)
+end
+
+# Creates rooms for each hotel
+hotels.each_with_index do |hotel, index|
+  20.times do
+    Room.create!(rate: Faker::Commerce.price, number: Faker::Number.digit , hotel_id: hotel.id )
+  end
+end
+
+# Creates bookings
+20.times do
+  Booking.create!(guest_id: rand(1..10) , room_id: rand(1..200) )
+end
