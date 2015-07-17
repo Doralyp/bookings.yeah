349d16ae9d550df010bf83d1b729e9ef7ac4e4f9
Doraly Pantaleon <doralypantaleon@gmail.com>
2015-07-16 22:10:12 -0400

    created migrations and models

---
+++ app/models/hotel.rb
@@ -0,0 +1,6 @@
+class Hotel < ActiveRecord::Base
+  has_many :rooms
+  has_many :bookings, through: :rooms
+  has_many :guests, through: :rooms
+  has_many :booked_guests, through: :bookings, source: :guest
+end
