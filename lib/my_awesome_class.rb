94e5ff0ff7aa2e6db08ccb2c7f84e016e21b2389
Doraly Pantaleon <doralypantaleon@gmail.com>
2015-07-17 22:45:10 -0400

    Adds a second time counter

--- lib/my_awesome_class.rb
+++ lib/my_awesome_class.rb
@@ -1,14 +1,17 @@
 require 'benchmark'
 
 class Myawesomeclass
+
+  puts "*" * 50
   puts "This report shows the user CPU time, system CPU time, the sum of the user and system CPU times, and the elapsed real time. The unit of time is seconds."
   Benchmark.bm(10) do |x|
    x.report("includes:") {Room.includes(:bookings)}
    x.report("joins:") {Room.joins(:bookings)}
    x.report("each:") {Room.all.each do |room| room.bookings end}
-   end
+  end
 
-  puts "Here is my custom benchmark in seconds using Ruby's Time Module"
+  puts "*" * 50
+  puts "Here is my custom benchmark in seconds using Ruby's Time Module:"
   start1 = Time.now
   Room.includes(:bookings)
   duration1 = Time.now - start1
@@ -17,13 +20,14 @@ class Myawesomeclass
   start2 = Time.now
   Room.joins(:bookings)
   duration2 = Time.now - start2
-  p "Joins take #{duration2} seconds"
+  p "Joins takes #{duration2} seconds"
 
   start3 = Time.now
   Room.all.each do |room|
     room.bookings
   end
   duration3 = Time.now - start3
-  p "Each take #{duration3} seconds"
+  p "Each takes #{duration3} seconds"
+  p "Joins are the most efficient in terms of relative runtime speeds with #{duration2} seconds"
 
 end
